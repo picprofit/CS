ee4c72a42a6e76a0b8657f91f8eb24ce74a3ebfa
Konstantin <picprofit@gmail.com>
2019-12-07 00:28:29 +0300

    new design in progress

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,113 +1,99 @@
-import React, { useState } from 'react';
-import { AppBar, Box, InputBase, Toolbar } from '@material-ui/core';
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
-import SearchIcon from '@material-ui/icons/Search';
-import { fade, makeStyles } from '@material-ui/core/styles';
+import React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import Box from '@material-ui/core/Box';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import HelpIcon from '@material-ui/icons/Help';
+import Hidden from '@material-ui/core/Hidden';
+import IconButton from '@material-ui/core/IconButton';
+import Link from '@material-ui/core/Link';
+import MenuIcon from '@material-ui/icons/Menu';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import Tab from '@material-ui/core/Tab';
+import Tabs from '@material-ui/core/Tabs';
+import Toolbar from '@material-ui/core/Toolbar';
+import Tooltip from '@material-ui/core/Tooltip';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
 
-const Header = ({ setSearchQuery: updateGlobalSearchQuery }) => {
-  const [searchQuery, setSearchQuery] = useState('');
+const lightColor = 'rgba(255, 255, 255, 0.7)';
 
-  const useStyles = makeStyles(theme => {
-    // console.log(theme);
-    return {
-      headingTitle: {
-        color: theme.palette.primary.contrastText
-      },
-      search: {
-        borderRadius: theme.shape.borderRadius,
-        backgroundColor: fade(theme.palette.common.white, 0.15),
-        '&:hover': {
-          backgroundColor: fade(theme.palette.common.white, 0.25)
-        },
-        [theme.breakpoints.up('sm')]: {
-          marginLeft: theme.spacing(1),
-          width: 'auto'
-        }
-      },
-      searchIcon: {
-        width: theme.spacing(7)
-      },
-      inputRoot: {
-        color: 'inherit'
-      },
-      inputInput: {
-        padding: theme.spacing(1, 1, 1, 7),
-        transition: theme.transitions.create('width'),
-        width: '100%',
-        [theme.breakpoints.up('sm')]: {
-          width: 120,
-          '&:focus': {
-            width: 200
-          }
-        }
-      }
-    };
-  });
+const styles = theme => ({
+  secondaryBar: {
+    zIndex: 0
+  },
+  menuButton: {
+    marginLeft: -theme.spacing(1)
+  },
+  iconButtonAvatar: {
+    padding: 4
+  },
+  link: {
+    textDecoration: 'none',
+    color: lightColor,
+    '&:hover': {
+      color: theme.palette.common.white
+    }
+  },
+  button: {
+    borderColor: lightColor
+  }
+});
 
-  const StyledTitle = styled(Link)`
-    font-size: 2rem;
-    font-weight: 300;
-    text-decoration: none;
-  `;
-  const StyledSubTitle = styled.span`
-    font-size: 1.2rem;
-  `;
-  const StyledBox = styled(Box)`
-    flex-grow: 1;
-  `;
-  const StyledSearch = styled.div`
-    position: relative;
-    margin-left: 0;
-    width: 100%;
-  `;
-  const StyledSearchIcon = styled(SearchIcon)`
-    height: 100%;
-    position: absolute;
-    pointer-events: none;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    top: 5px;
-    left: 10px;
-  `;
-  const classes = useStyles();
+function Header(props) {
+  const { classes, onDrawerToggle } = props;
+
   return (
-    <AppBar>
-      <Toolbar>
-        <StyledBox mt={3} mb={3}>
-          <StyledTitle to="/" className={classes.headingTitle}>
-            WordPress/React{' '}
-            <StyledSubTitle>personal stylesheet collection</StyledSubTitle>
-          </StyledTitle>
-        </StyledBox>
-        <div className={classes.search}>
-          <StyledSearch className={classes.searchIcon}>
-            <StyledSearchIcon />
-          </StyledSearch>
-          <form
-            onSubmit={e => {
-              e.preventDefault();
-              updateGlobalSearchQuery(searchQuery);
-            }}
-          >
-            <InputBase
-              placeholder="Search…"
-              classes={{
-                root: classes.inputRoot,
-                input: classes.inputInput
-              }}
-              inputProps={{ 'aria-label': 'search' }}
-              value={searchQuery}
-              onChange={e => {
-                setSearchQuery(e.target.value);
-              }}
-            />
-          </form>
-        </div>
-      </Toolbar>
-    </AppBar>
+    <>
+      <AppBar
+        component="div"
+        className={classes.secondaryBar}
+        color="primary"
+        position="static"
+        elevation={0}
+      >
+        <Box pt={2}>
+          <Toolbar>
+            <Grid container alignItems="center" spacing={1}>
+              <Grid item xs>
+                <Typography color="inherit" variant="h5" component="h1">
+                  Authentication
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Tooltip title="Help">
+                  <IconButton color="inherit">
+                    <HelpIcon />
+                  </IconButton>
+                </Tooltip>
+              </Grid>
+            </Grid>
+          </Toolbar>
+        </Box>
+      </AppBar>
+      <AppBar
+        component="div"
+        className={classes.secondaryBar}
+        color="primary"
+        position="static"
+        elevation={0}
+      >
+        <Tabs value={0} textColor="inherit">
+          <Tab textColor="inherit" label="Users" />
+          <Tab textColor="inherit" label="Sign-in method" />
+          <Tab textColor="inherit" label="Templates" />
+          <Tab textColor="inherit" label="Usage" />
+        </Tabs>
+      </AppBar>
+    </>
   );
+}
+
+Header.propTypes = {
+  classes: PropTypes.object.isRequired,
+  onDrawerToggle: PropTypes.func.isRequired
 };
 
-export default Header;
+export default withStyles(styles)(Header);
