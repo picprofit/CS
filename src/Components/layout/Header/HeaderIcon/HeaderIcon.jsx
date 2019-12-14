b156810d220abdca7568307f99ee9333985f0dc0
Konstantin <picprofit@gmail.com>
2019-12-14 13:13:42 +0300

    add: random icon

--- src/Components/layout/Header/HeaderIcon/HeaderIcon.jsx
+++ src/Components/layout/Header/HeaderIcon/HeaderIcon.jsx
@@ -3,18 +3,25 @@ import Tooltip from '@material-ui/core/Tooltip';
 import IconButton from '@material-ui/core/IconButton';
 import HelpIcon from '@material-ui/icons/Help';
 
+import iconsList from './iconsList';
+
 const HeaderIcon = () => {
   const [title, setTitle] = useState('Hello');
   const [count, setCount] = useState(0);
+  const TagName = iconsList[Math.floor(Math.random() * iconsList.length)];
+
   return (
-    <Tooltip title={title} onClick={() => {
-      setCount(count + 1);
-      if(count > 10) {
-        setTitle(`Hello! You clicked ${count} times. Why?`)
-      }
-    }}>
+    <Tooltip
+      title={title}
+      onClick={() => {
+        setCount(count + 1);
+        if (count > 10) {
+          setTitle(`Hello! You clicked ${count} times. Why?`);
+        }
+      }}
+    >
       <IconButton color="inherit">
-        <HelpIcon />
+        {count === 0 ? <HelpIcon /> : <TagName />}
       </IconButton>
     </Tooltip>
   );
---
+++ src/Components/layout/Header/HeaderIcon/iconsList.js
@@ -0,0 +1,14 @@
+import AcUnitIcon from '@material-ui/icons/AcUnit';
+import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
+import AccessibilityNewIcon from '@material-ui/icons/AccessibilityNew';
+import AdbIcon from '@material-ui/icons/Adb';
+import HelpIcon from '@material-ui/core/SvgIcon/SvgIcon';
+
+
+export default [
+  HelpIcon,
+  AcUnitIcon,
+  AccessAlarmIcon,
+  AccessibilityNewIcon,
+  AdbIcon,
+];
