2f2e54add2d4efdc88dfee1be5db845f0eab37fc
Konstantin <picprofit@gmail.com>
2019-12-14 13:35:38 +0300

    add: random icons

--- src/Components/layout/Header/HeaderIcon/iconsList.js
+++ src/Components/layout/Header/HeaderIcon/iconsList.js
@@ -3,6 +3,53 @@ import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
 import AccessibilityNewIcon from '@material-ui/icons/AccessibilityNew';
 import AdbIcon from '@material-ui/icons/Adb';
 import HelpIcon from '@material-ui/core/SvgIcon/SvgIcon';
+import AddToHomeScreenIcon from '@material-ui/icons/AddToHomeScreen';
+import AirplanemodeActiveIcon from '@material-ui/icons/AirplanemodeActive';
+import AirportShuttleIcon from '@material-ui/icons/AirportShuttle';
+import AndroidIcon from '@material-ui/icons/Android';
+import AppleIcon from '@material-ui/icons/Apple';
+import AssistantPhotoIcon from '@material-ui/icons/AssistantPhoto';
+import AttachFileIcon from '@material-ui/icons/AttachFile';
+import BathtubIcon from '@material-ui/icons/Bathtub';
+import BeachAccessIcon from '@material-ui/icons/BeachAccess';
+import BlurCircularIcon from '@material-ui/icons/BlurCircular';
+import Brightness5Icon from '@material-ui/icons/Brightness5';
+import Brightness7Icon from '@material-ui/icons/Brightness7';
+import BrushIcon from '@material-ui/icons/Brush';
+import BugReportIcon from '@material-ui/icons/BugReport';
+import CakeIcon from '@material-ui/icons/Cake';
+import CallIcon from '@material-ui/icons/Call';
+import CameraIcon from '@material-ui/icons/Camera';
+import ChildFriendlyIcon from '@material-ui/icons/ChildFriendly';
+import CloudIcon from '@material-ui/icons/Cloud';
+import ColorLensIcon from '@material-ui/icons/ColorLens';
+import CommuteIcon from '@material-ui/icons/Commute';
+import ComputerIcon from '@material-ui/icons/Computer';
+import DeckIcon from '@material-ui/icons/Deck';
+import DirectionsBikeIcon from '@material-ui/icons/DirectionsBike';
+import DirectionsBoatIcon from '@material-ui/icons/DirectionsBoat';
+import DirectionsBusIcon from '@material-ui/icons/DirectionsBus';
+import DirectionsCarIcon from '@material-ui/icons/DirectionsCar';
+import DirectionsRailwayIcon from '@material-ui/icons/DirectionsRailway';
+import DirectionsSubwayIcon from '@material-ui/icons/DirectionsSubway';
+import EmojiEmotionsIcon from '@material-ui/icons/EmojiEmotions';
+import EmojiEventsIcon from '@material-ui/icons/EmojiEvents';
+import EmojiTransportationIcon from '@material-ui/icons/EmojiTransportation';
+import EmojiFoodBeverageIcon from '@material-ui/icons/EmojiFoodBeverage';
+import FitnessCenterIcon from '@material-ui/icons/FitnessCenter';
+import HeadsetIcon from '@material-ui/icons/Headset';
+import HighlightIcon from '@material-ui/icons/Highlight';
+import HotTubIcon from '@material-ui/icons/HotTub';
+import LaptopMacIcon from '@material-ui/icons/LaptopMac';
+import LocalDiningIcon from '@material-ui/icons/LocalDining';
+import LocalFloristIcon from '@material-ui/icons/LocalFlorist';
+import PoolIcon from '@material-ui/icons/Pool';
+import PrintIcon from '@material-ui/icons/Print';
+import RedditIcon from '@material-ui/icons/Reddit';
+import SportsBaseballIcon from '@material-ui/icons/SportsBaseball';
+import SportsKabaddiIcon from '@material-ui/icons/SportsKabaddi';
+import WatchIcon from '@material-ui/icons/Watch';
+
 
 
 export default [
@@ -11,4 +58,50 @@ export default [
   AccessAlarmIcon,
   AccessibilityNewIcon,
   AdbIcon,
+  AddToHomeScreenIcon,
+  AirplanemodeActiveIcon,
+  AirportShuttleIcon,
+  AndroidIcon,
+  AppleIcon,
+  AssistantPhotoIcon,
+  AttachFileIcon,
+  BathtubIcon,
+  BeachAccessIcon,
+  BlurCircularIcon,
+  Brightness5Icon,
+  Brightness7Icon,
+  BrushIcon,
+  BugReportIcon,
+  CakeIcon,
+  CallIcon,
+  CameraIcon,
+  ChildFriendlyIcon,
+  CloudIcon,
+  ColorLensIcon,
+  CommuteIcon,
+  ComputerIcon,
+  DeckIcon,
+  DirectionsBikeIcon,
+  DirectionsBoatIcon,
+  DirectionsBusIcon,
+  DirectionsCarIcon,
+  DirectionsRailwayIcon,
+  DirectionsSubwayIcon,
+  EmojiEmotionsIcon,
+  EmojiEventsIcon,
+  EmojiTransportationIcon,
+  EmojiFoodBeverageIcon,
+  FitnessCenterIcon,
+  HeadsetIcon,
+  HighlightIcon,
+  HotTubIcon,
+  LaptopMacIcon,
+  LocalDiningIcon,
+  LocalFloristIcon,
+  PoolIcon,
+  PrintIcon,
+  RedditIcon,
+  SportsBaseballIcon,
+  SportsKabaddiIcon,
+  WatchIcon,
 ];
