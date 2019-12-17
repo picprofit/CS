e515ff91d638cb34d6a42950cde91834e6f77832
Konstantin <picprofit@gmail.com>
2019-12-17 11:46:54 +0300

    upd: loader

--- src/Components/layout/Loader/Loader.jsx
+++ src/Components/layout/Loader/Loader.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
 const Loader = ({ alt = false }) => {
+  // empty tags need for pure css loader works
   return (
     <div className={alt ? 'loader_ripple-alt' : 'loader_ripple'}>
-      <div></div>
-      <div></div>
+      <div />
+      <div />
     </div>
   );
 };
