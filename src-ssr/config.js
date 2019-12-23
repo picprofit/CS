21da77f970f3d675b2903ca5c9c00eae93df22d7
Konstantin <picprofit@gmail.com>
2019-12-23 15:41:07 +0300

    upd: all ssr-independent moved

--- src-ssr/config.js
+++ src-ssr/config.js
@@ -2,4 +2,4 @@ const apiUrl =
   process.env.NODE_ENV === 'production'
     ? 'https://csback.kskonovalov.me/graphql'
     : 'http://localhost/cs/backend/graphql';
-export { apiUrl };
+export default apiUrl;
