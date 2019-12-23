fc92c942a7d3e5f9a30660618fa857c75fa32c02
Konstantin <picprofit@gmail.com>
2019-12-23 15:43:29 +0300

    upd: initial moved to _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import App from 'next/app';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { Provider } from 'react-redux';
+import 'nprogress/nprogress.css';
+
+import store from '../src-ssr/store';
+import theme from '../src-ssr/theme';
+import '../src-ssr/assets/style.scss';
 
 class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
-      <>
-        <Component {...pageProps} />
-      </>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </Provider>
     );
   }
 }
