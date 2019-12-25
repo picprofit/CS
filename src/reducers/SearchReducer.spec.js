42f1de0fb5f1f8263ca0e8894ce3dd90af4e2288
Konstantin <picprofit@gmail.com>
2019-12-25 11:30:00 +0300

    add: SearchReducer test

--- src/reducers/SearchReducer.spec.js
+++ src/reducers/SearchReducer.spec.js
@@ -1,12 +1,18 @@
+import expect from 'expect';
+
 import { SET_SEARCH } from '../constants';
+import SearchReducer from './SearchReducer';
 
-const SearchReducer = (state = {}, action) => {
-  switch (action.type) {
-    case SET_SEARCH:
-      return action.search;
-    default:
-      return state;
-  }
-};
-
-export default SearchReducer;
+describe('SearchReducer test', () => {
+  it('SearchReducer should replace search string', () => {
+    const searchString = 'Old search string';
+    const newSearchString = 'New search string';
+    const expectedState = 'New search string';
+    expect(
+      SearchReducer(searchString, {
+        type: SET_SEARCH,
+        search: newSearchString
+      })
+    ).toEqual(expectedState);
+  });
+});
