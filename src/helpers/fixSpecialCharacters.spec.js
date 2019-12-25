eeda29403d041c3a0f81eca66276a8084314d89e
Konstantin <picprofit@gmail.com>
2019-12-25 11:10:19 +0300

    add: helpers fixSpecialCharacters tests

--- src/helpers/fixSpecialCharacters.spec.js
+++ src/helpers/fixSpecialCharacters.spec.js
@@ -8,4 +8,19 @@ describe('fixSpecialCharacters:', () => {
     const resultString = '&lt;script&gt;';
     expect(resultString).toEqual(fixSpecialCharacters(string));
   });
+  it('Should replace few <<< to &lt;&lt;&lt;', () => {
+    const string = '<<<script>';
+    const resultString = '&lt;&lt;&lt;script&gt;';
+    expect(resultString).toEqual(fixSpecialCharacters(string));
+  });
+  it('Should replace few >>> to &gt;&gt;&gt;', () => {
+    const string = '<script>>>';
+    const resultString = '&lt;script&gt;&gt;&gt;';
+    expect(resultString).toEqual(fixSpecialCharacters(string));
+  });
+  it('Should not change other spec symbols like " \' & $', () => {
+    const string = '" \' & $';
+    const resultString = '" \' & $';
+    expect(resultString).toEqual(fixSpecialCharacters(string));
+  });
 });
