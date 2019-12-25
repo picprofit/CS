433345f27b6500a6291c654f7ae2b57307d44869
Konstantin <picprofit@gmail.com>
2019-12-25 11:19:47 +0300

    add: FilterReducer test

---
+++ src/reducers/FilterReducer.spec.js
@@ -0,0 +1,17 @@
+import expect from 'expect';
+import { SET_FILTER } from '../constants';
+import FilterReducer from './FilterReducer';
+
+describe('FilterReducer test', () => {
+  it('Should update filter', () => {
+    const state = 'Old string';
+    const newFilter = 'Some Search String';
+    const expectedState = 'Some Search String';
+    expect(
+      FilterReducer(state, {
+        type: SET_FILTER,
+        filter: newFilter
+      })
+    ).toEqual(expectedState);
+  });
+});
