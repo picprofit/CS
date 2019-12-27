bebb6f937598a709f64561c3a91ebbd8b8f95525
Konstantin <picprofit@gmail.com>
2019-12-27 17:26:56 +0300

    upd: src renamed

--- src/Components/layout/Layout.jsx
+++ src/Components/layout/Layout.jsx
@@ -1,67 +1,48 @@
 import React, { useState } from 'react';
 import { CssBaseline, Hidden } from '@material-ui/core';
-import { ThemeProvider } from '@material-ui/core/styles';
-import { Provider } from 'react-redux';
-import 'nprogress/nprogress.css';
-
-import Navigator from './Navigator/Navigator';
-import { drawerWidth } from '../styles';
-import Header from './Header';
-// import Content from './Content';
-// import Copyright from './Copyright';
-
-import store from '../../store';
-import theme from '../../theme';
-import '../../assets/style.scss';
-
-import { connect } from 'react-redux';
 import { withStyles } from '@material-ui/core/styles';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { ApolloProvider } from 'react-apollo';
-import ApolloClient from 'apollo-boost';
-import Helmet from 'react-helmet';
 
-import Posts from './pages/Posts';
-import Post from './pages/Post';
-import NotFound from './pages/NotFound';
-import Category from './pages/Category';
-import Categories from './pages/Categories';
-import Search from './pages/Search';
+import styles, { drawerWidth } from '../styles';
+import Navigator from './Navigator';
+import Header from './Header';
+import Content from './Content';
+import Copyright from './Copyright';
 
-import styles  from './styles';
-import { apiUrl } from '../config';
-import { setTitle } from '../actions';
-import Layout from './layout/Layout';
-
-const client = new ApolloClient({
-  uri: apiUrl
-});
-
-
-const Layout = ({ children }) => {
+const Layout = ({ classes, children }) => {
   const [mobileOpen, setMobileOpen] = useState(false);
-
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
   };
   return (
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <ApolloProvider client={client}>
-        <div>
-          <CssBaseline />
-          <div>
-            <Header
+    <>
+      <div className={classes.root}>
+        <CssBaseline />
+        <nav className={classes.drawer}>
+          <Hidden smUp implementation="js">
+            <Navigator
+              PaperProps={{ style: { width: drawerWidth } }}
+              variant="temporary"
+              open={mobileOpen}
+              onClose={handleDrawerToggle}
               onDrawerToggle={handleDrawerToggle}
-              mobileOpen={mobileOpen}
             />
-            <main>{children}</main>
-          </div>
+          </Hidden>
+          <Hidden xsDown implementation="css">
+            <Navigator PaperProps={{ style: { width: drawerWidth } }} />
+          </Hidden>
+        </nav>
+        <div className={classes.app}>
+          <Header onDrawerToggle={handleDrawerToggle} mobileOpen={mobileOpen} />
+          <main className={classes.main}>
+            <Content>{children}</Content>
+          </main>
+          <footer className={classes.footer}>
+            <Copyright />
+          </footer>
         </div>
-        </ApolloProvider>
-      </ThemeProvider>
-    </Provider>
+      </div>
+    </>
   );
 };
 
-export default Layout;
+export default withStyles(styles)(Layout);
