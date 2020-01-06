93fd63200f346b01b5560276ee01c5e67c0fb66a
Konstantin <picprofit@gmail.com>
2020-01-06 17:15:29 +0300

    upd: Copyright

--- src/Components/layout/Copyright/Copyright.jsx
+++ src/Components/layout/Copyright/Copyright.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
 
 const Copyright = () => {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       Created by{' '}
-      <a color="inherit" href="https://kskonovalov.me/">
-        me
-      </a>{' '}
+      <Link href="https://kskonovalov.me/" color="inherit" target="_blank">
+        K.Konovalov
+      </Link>{' '}
       in 2019
     </Typography>
   );
